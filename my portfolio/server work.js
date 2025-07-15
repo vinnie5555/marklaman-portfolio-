d7a25bfe823fee2542e08bce9139a422fe24ecf5
vinnie5555 <vincentkamau137@gmail.com>
2025-07-15 09:55:43 -0400

    Add files via upload

---
+++ my portfolio/server work.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "devmark-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/script.js",
+  "/assets/icons/icon-192.png",
+  "/assets/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
